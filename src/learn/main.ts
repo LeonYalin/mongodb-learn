3c9e7c8c4938ef3d7546b69da9306abc25e4ba5b
Leon Yalin <yelinichevl@gmail.com>
2022-09-13 21:42:40 +0300

    rename files

--- src/learn/main.ts
+++ src/learn/main.ts
@@ -1,12 +1,12 @@
-import basicCRUD from './basicCRUD';
-import create from './createOperations';
-import deleteOp from './deleteOperations';
-import introduction from './introduction';
-import read from './readOperations';
-import structuringDocuments from './structuringDocuments';
-import update from './updateOperations';
-import geospatial from './workingWithGeospatialData';
-import indexes from './workingWithIndexes';
+import basicCRUD from './2.basicCRUD';
+import create from './4.createOperations';
+import deleteOp from './7.deleteOperations';
+import introduction from './1.introduction';
+import read from './5.readOperations';
+import structuringDocuments from './3.structuringDocuments';
+import update from './6.updateOperations';
+import geospatial from './9.workingWithGeospatialData';
+import indexes from './8.workingWithIndexes';
 
 export function main() {
   introduction();
